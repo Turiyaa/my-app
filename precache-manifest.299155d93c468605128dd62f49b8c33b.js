1c713577e9a72d601109f3f2f9feed014b58f475
rango <nneopane@oswego.edu>
2019-04-08 18:48:11 -0400

    Updates

---
+++ precache-manifest.299155d93c468605128dd62f49b8c33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.1cff5780.js"
+  },
+  {
+    "revision": "5176341d42a9761db90a",
+    "url": "/my-app/static/js/main.3b3bd1f1.chunk.js"
+  },
+  {
+    "revision": "8f7f242aa2caecd0bead",
+    "url": "/my-app/static/js/2.e423ff8f.chunk.js"
+  },
+  {
+    "revision": "5176341d42a9761db90a",
+    "url": "/my-app/static/css/main.9e90ec50.chunk.css"
+  },
+  {
+    "revision": "8f7f242aa2caecd0bead",
+    "url": "/my-app/static/css/2.5bc987fd.chunk.css"
+  },
+  {
+    "revision": "f7022cb9b46e497477d8fcb1847b2d2e",
+    "url": "/my-app/index.html"
+  }
+];
